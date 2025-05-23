11536fc89908fcd4e695cc69d120c502d1b45d0f
Vitor José Stähelin <vitorjosestahelin@gmail.com>
2025-05-23 10:07:35 -0300

    feat: Ajustar configuração de plugins e base no Vite
    Removido o plugin vite-plugin-gh-pages da configuração do Vite e ajustada a definição da base do projeto para ser dinâmica, dependendo do modo de produção ou desenvolvimento. Essas alterações visam otimizar a configuração do ambiente de desenvolvimento.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -10,12 +9,10 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-    ghPages(),
-  ].filter(Boolean),
-  base: "/",
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
+  base: mode === "production" ? "/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
